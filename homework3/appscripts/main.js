f7c32971ec9a4d586aa292eb92a57d8f6cd7a9e8
yuenteo <110802488+yuenteo@users.noreply.github.com>
2022-08-08 12:57:45 +0800

    Add homework3

---
+++ homework3/appscripts/main.js
@@ -0,0 +1,12 @@
+// main.js
+
+console.log(`yo`);
+
+/* alternatively, this also works:
+let articleFont = document.getElementById("newFont");
+articleFont.style.fontFamily = "Tahoma,Geneva,sans-serif"; */
+document.getElementById("newFont").style.fontFamily = "Tahoma,Geneva,sans-serif";
+
+/* let headerPosition = document.getElementById("centerHeader");
+headerPosition.style.textAlign = "center"; */
+document.getElementById("centerHeader").style.textAlign = "center";
